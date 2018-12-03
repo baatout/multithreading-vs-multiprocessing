5908f02151a052eb3ed82b71911326bad5b697d0
baatout <mohamed-amine.baatout@contentsquare.com>
2018-12-03 16:35:48 +0100

    Update io experiment

--- serial_comparison_io.py
+++ serial_comparison_io.py
@@ -1,5 +1,26 @@
 import time
+import urllib.request
 from concurrent.futures import ThreadPoolExecutor
+
+urls = ['http://www.discovertunisia.com',
+        'http://whc.unesco.org/en/list/38',
+        'https://www.youtube.com/watch?v=sHORcz5nqIc',
+        'https://www.youtube.com/watch?v=dyHjCwKoNn8'
+        'http://www.tourismtunisia.com/10-adventurous-things-to-do-in-tunisia/',
+        'http://wowtravel.me/top-10-things-to-do-in-tunisia/',
+        'http://en.wikipedia.org/wiki/Tataouine',
+        'http://en.wikipedia.org/wiki/Carthage',
+        'http://en.wikipedia.org/wiki/Hannibal',
+        'http://www.discovertunisia.com',
+        'http://whc.unesco.org/en/list/38',
+        'https://www.youtube.com/watch?v=sHORcz5nqIc',
+        'https://www.youtube.com/watch?v=dyHjCwKoNn8'
+        'http://www.tourismtunisia.com/10-adventurous-things-to-do-in-tunisia/',
+        'http://wowtravel.me/top-10-things-to-do-in-tunisia/',
+        'http://en.wikipedia.org/wiki/Tataouine',
+        'http://en.wikipedia.org/wiki/Carthage',
+        'http://en.wikipedia.org/wiki/Hannibal'
+        ]
 
 
 def multithreading(func, args, workers):
@@ -8,16 +29,18 @@ def multithreading(func, args, workers):
     return list(res)
 
 
-def simulate_io(x):
-    print('I am', x)
-    time.sleep(3)
+def load_url(x):
+    # print('I am', x)
+    with urllib.request.urlopen(urls[x], timeout=20) as conn:
+        return conn.read()
 
 
-n_jobs = 4
+n_jobs = len(urls)
 
 marker = time.time()
-for i in range(n_jobs): simulate_io(i)
+for i in range(n_jobs): load_url(i)
 print("Serial spent", time.time() - marker)
-marker = time.time()
-multithreading(simulate_io, range(n_jobs), 4)
-print("Multithreading spent", time.time() - marker)
+for n_threads in [4, 8, 16]:
+    marker = time.time()
+    multithreading(load_url, range(n_jobs), n_threads)
+    print("Multithreading {} spent".format(n_threads), time.time() - marker)
